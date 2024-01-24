b3671c9e62f168d22802d30846149c11c87c93d9
First Fractal <firstfractal512@gmail.com>
2024-01-24 15:08:17 -0600

    made it only spawn in a clone if the current not is not a clone

--- ZIMGlobalNPC.cs
+++ ZIMGlobalNPC.cs
@@ -10,17 +10,28 @@ namespace ZombieInvasionMod
         //function that runs whenever a npc spawns in
         public override void OnSpawn(NPC npc, IEntitySource source)
         {
+            
             //loop through every vanilla zombie
             foreach (int zombie in ffVar.zombies.allZombies)
             {
                 //check if the current npc is a zombie
                 if (npc.type == zombie)
                 {
-                    //get a random normal zombie from the list
-                    int randZom = ffVar.zombies.normalZombies[Main.rand.Next(ffVar.zombies.normalZombies.Length)];
+                    //check if the current zombie is a clone. If not, then give it a clone
+                    if (source is EntitySource_Misc misc && misc.Context.Equals("Zombie Clone"))
+                    {
+                        ffFunc.Talk("Prevent a zombie clone from spawning in", Color.Orange);
+                    }
+                    else
+                    {
+                        //get a random normal zombie from the list
+                        int randZom = ffVar.zombies.normalZombies[Main.rand.Next(ffVar.zombies.normalZombies.Length)];
 
-                    //spawn in a new zombie on top of the current one with the reason being a clone
-                    NPC.NewNPC(new EntitySource_Misc("Zombie Clone"), (int)npc.position.X, (int)npc.position.Y, randZom);
+                        //spawn in a new zombie on top of the current one with the reason being a clone
+                        NPC.NewNPC(new EntitySource_Misc("Zombie Clone"), (int)npc.position.X, (int)npc.position.Y, randZom);
+
+                        ffFunc.Talk("Spawned in a Zombie Clone", Color.Red);
+                    }
                 }
             }
             base.OnSpawn(npc, source);
